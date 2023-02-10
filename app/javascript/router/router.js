c23281918f6a65bc1403b69b480c909c94764850
keita-abe1022 <keita.abe1022@gmail.com>
2023-02-10 09:47:04 +0000

    routerの修正

--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // コーヒー投稿一覧(ホーム画面)
-import CoffeeHome from '../pages/CoffeeHome.vue'
-import CoffeeCreate from  '../pages/CoffeeCreate.vue'
-import CoffeeEdit from '../pages/CoffeeEdit.vue'
+import CoffeeHome from '../pages/coffees/CoffeeHome.vue'
+import CoffeeCreate from  '../pages/coffees/CoffeeCreate.vue'
+import CoffeeEdit from '../pages/coffees/CoffeeEdit.vue'
 // 新規登録・ログインページ
-import Signup from '../pages/Signup.vue'
-import Signin from '../pages/Signin.vue'
+import Signup from '../pages/users/Signup.vue'
+import Signin from '../pages/users/Signin.vue'
 import UserShow from '../pages/users/UserShow.vue'
 
 Vue.use(VueRouter)
